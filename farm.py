645164859d6d923d41b7133294a11a24bc821541
Davi Queiroz <daviqueiroz26@proton.me>
2026-04-02 21:56:32 -0300

    add init and attributes

--- farm.py
+++ farm.py
@@ -1,1 +1,10 @@
 class Farm:
+
+    def __init__(self, farm_name):
+
+        self.farm_name = farm_name
+        self.coins = 0
+
+        self.seeds = self._init_seeds()
+        self.crops = self._init_crops()
+        self.seed_prices = self._init_seed_prices()
